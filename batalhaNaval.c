a97c89a53b30035b5f7bd855a540227db0bfa34f
Ronaldy Guimarães <ronaldyguimaraes.rgg@gmail.com>
2025-03-17 22:03:30 -0300

    update

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -2,28 +2,27 @@
 
 int main() {
     int tabuleiro[10][10] = {0};
-    char colunas[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-    int navioHorizontal[3] = {3, 3, 3};
-    int navioVertical[3] = {3, 3, 3};
-
-
+    char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    int navio_h[3] = {3, 3, 3};
+    int navio_v[3] = {3, 3, 3};
 
     for (int i = 0; i < 3; i++) {
-        tabuleiro[2][3 + i] = navioHorizontal[i];
+        tabuleiro[3][4 + i] = navio_h[i];
     }
 
     for (int i = 0; i < 3; i++) {
-        tabuleiro[5 + i][7] = navioVertical[i];
+        tabuleiro[6 + i][2] = navio_v[i];
     }
 
-    printf("  ");
+    
+    printf("  ");  
     for (int i = 0; i < 10; i++) {
-        printf("%c ", colunas[i]);
+        printf("%c ", linha[i]);  
     }
     printf("\n");
 
     for (int i = 0; i < 10; i++) {
-        printf("%d ", i + 1);
+        printf("%2d ", i + 1);  
         for (int j = 0; j < 10; j++) {
             printf("%d ", tabuleiro[i][j]);
         }
